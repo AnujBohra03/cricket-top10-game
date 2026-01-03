1e8612736fbb402742014135bd2467ad145df2de
AnujBohra03 <AnujBohra03@github.com>
2026-01-03 15:16:51 +0530

    Deploy website on git

--- frontend/cricket-top10-ui/vite.config.ts
+++ frontend/cricket-top10-ui/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/cricket-top10-game/",
   plugins: [react()],
-})
+});
+
